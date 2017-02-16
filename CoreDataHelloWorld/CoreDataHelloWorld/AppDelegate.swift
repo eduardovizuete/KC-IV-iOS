80ab83fd72a3f2e77079db035380fb758cd307ea
Diego Freniche Brito <diego@freniche.com>
2017-02-16 10:15:37 +0100

    Added CoreDataHelloWorld example

---
+++ CoreDataHelloWorld/CoreDataHelloWorld/AppDelegate.swift
@@ -0,0 +1,61 @@
+import UIKit
+import CoreData
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDelegate {
+    
+    var window: UIWindow?
+    let coreDataManager = CoreDataManager()
+    var context: NSManagedObjectContext?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        let splitViewController = self.window!.rootViewController as! UISplitViewController
+        let navigationController = splitViewController.viewControllers[splitViewController.viewControllers.count-1] as! UINavigationController
+        navigationController.topViewController!.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem
+        splitViewController.delegate = self
+
+        let masterNavigationController = splitViewController.viewControllers[0] as! UINavigationController
+        let controller = masterNavigationController.topViewController as! MasterViewController
+        self.context = self.coreDataManager.persistentContainer(dbName: "CoreDataHelloWorld").viewContext
+        
+        testZone()
+        
+        controller.managedObjectContext = self.context
+
+        
+        return true
+    }
+
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        self.coreDataManager.saveContext(context: self.context!)
+    }
+
+    // MARK: - Split view
+
+    func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController:UIViewController, onto primaryViewController:UIViewController) -> Bool {
+        guard let secondaryAsNavController = secondaryViewController as? UINavigationController else { return false }
+        guard let topAsDetailController = secondaryAsNavController.topViewController as? DetailViewController else { return false }
+        if topAsDetailController.detailItem == nil {
+            // Return true to indicate that we have handled the collapse by doing nothing; the secondary controller will be discarded.
+            return true
+        }
+        return false
+    }
+   
+    func testZone() {
+        let ev1 = Event(context: self.context!)
+        ev1.timestamp = NSDate()
+        
+        let ev2 = Event(context: self.context!)
+        ev2.timestamp = NSDate()
+
+        self.coreDataManager.saveContext(context: self.context!)
+        
+        let diego: Person = Person(context: self.context!)
+        
+    }
+    
+}
+
